29c213aafc36dd31cda38f6a33e8142002149faf
Harun29 <ibrahimovic.harun01@gmail.com>
2024-01-18 23:44:01 +0100

    created read and update methods for vozilo

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+  public static void main(String[] args) {
+
+    Vozilo auto = new Vozilo("vw", "polo", 1999, 4600.0, "a54-f-569");
+
+    auto.updateField("proizvodjac", "audi");
+    auto.updateField("model", "a8");
+
+    System.out.println(auto);
+  }
 }
--- src/Vozilo.java
+++ src/Vozilo.java
@@ -1,21 +1,50 @@
 public class Vozilo {
+  private String proizvodjac;
+  private String model;
+  private int godiste;
+  private double cijena;
+  private String registracija;
+  private boolean rentano;
 
-    private String brend;
-    private String model;
-    private int godiste;
-    private double cijena;
-    private String registracija;
-    private boolean rentano;
-
-//    Konstruktor
-
-    public Vozilo(String brend, String model, int godiste, double cijena){
-      this.brend = brend;
-      this.model = model;
-      this.godiste = godiste;
-      this.cijena = cijena;
-      this.rentano = false;
+//  C
+  public Vozilo(String proizvodjac, String model, int godiste, double cijena, String registracija){
+    this.proizvodjac = proizvodjac;
+    this.model = model;
+    this.godiste = godiste;
+    this.cijena = cijena;
+    this.registracija = registracija;
+    this.rentano = false;
+  }
+//  R
+  @Override
+  public String toString(){
+    return this.proizvodjac + " " + this.model + " " + this.godiste + " " + this.cijena + " " + this.registracija + " " + this.rentano;
+  }
+//  U
+  public void updateField(String fieldName, Object value) {
+    switch (fieldName) {
+      case "proizvodjac":
+        this.proizvodjac = (String) value;
+        break;
+      case "model":
+        this.model = (String) value;
+        break;
+      case "godiste":
+        this.godiste = (int) value;
+        break;
+      case "cijena":
+        this.cijena = (double) value;
+        break;
+      case "registracija":
+        this.registracija = (String) value;
+        break;
+      case "rentano":
+        this.rentano = (boolean) value;
+        break;
+      default:
+        throw new IllegalArgumentException("Invalid field name: " + fieldName);
     }
+  }
 
 }
 
