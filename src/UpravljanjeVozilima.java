3484e5172b7b4c70c9870e688db6a306e7771229
Harun29 <ibrahimovic.harun01@gmail.com>
2024-01-19 17:12:20 +0100

    created class for manageing vehicles

---
+++ src/UpravljanjeVozilima.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+public class UpravljanjeVozilima {
+  public ArrayList<Vozilo> vozila= new ArrayList<Vozilo>();
+  public void dodajVozilo(Vozilo vozilo){
+    vozila.add(vozilo);
+  }
+  public void prikaziVozila(){
+    vozila.forEach(vozilo -> {
+      System.out.println(vozilo);
+    });
+  }
+  public Vozilo pronadjiVozilo(String registracija) {
+    return vozila.stream()
+            .filter(vozilo -> vozilo.registracija().equals(registracija))
+            .findFirst()
+            .orElse(null);
+  }
+}
